12ae920ef0252aa62066727aaadfdd64628f2343
andremeronen1 <andre@hkhk.edu.ee>
2024-10-04 11:16:41 +0300

    Add files via upload

---
+++ harjutus01.py
@@ -0,0 +1,18 @@
+# Andre Meronen
+# 04.10.24
+# Harjutus01
+
+print('"Cabernet Sauvignon" või \'Cabernet Sauvignon\'?')
+print('  @..@')
+print(' (----)')
+print('( \__/ )')
+print('^^ "" ^^')
+print()
+
+#int täisarv 123
+#str sõne "tekst"
+#float komaarv 1.23
+
+a = int(input("Lisa ruudu külje pikkus: "))
+s = a ** 2 #astendamine
+print("Vastus: ruudu pindala on ",s)
