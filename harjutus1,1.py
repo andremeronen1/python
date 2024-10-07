95a7ed202361f4bee68a73ed452135156e48f514
andremeronen1 <andre@hkhk.edu.ee>
2024-10-07 15:28:03 +0300

    Add files via upload

---
+++ harjutus1,1.py
@@ -0,0 +1,81 @@
+# Andre Meronen 07.10.2024
+# Harjutus 01.1
+import turtle
+
+# Muudan asukohta
+turtle.penup()
+turtle.goto(-300,200)
+turtle.pendown()
+
+# Kolmnurk
+turtle.fd(100)
+turtle.left(120)
+turtle.fd(100)
+turtle.left(120)
+turtle.fd(100)
+turtle.left(120)
+
+# Muudan asukohta jälle
+turtle.penup()
+turtle.goto(0,200)
+turtle.pendown()
+
+# A täht
+turtle.fd(50)
+turtle.left(60)
+turtle.fd(50)
+turtle.right(60)
+turtle.fd(50)
+turtle.right(60)
+turtle.fd(50)
+turtle.left(60)
+turtle.fd(50)
+turtle.left(120)
+turtle.fd(200)
+turtle.left(120)
+turtle.fd(200)
+
+turtle.penup()
+turtle.goto(120,250)
+turtle.pendown()
+
+turtle.right(60)
+turtle.fd(45)
+turtle.right(120)
+turtle.fd(50)
+turtle.right(120)
+turtle.fd(50)
+
+turtle.penup()
+turtle.goto(-100,200)
+turtle.pendown()
+
+turtle.left(60)
+turtle.fd(50)
+turtle.left(90)
+turtle.fd(50)
+turtle.right(90)
+turtle.fd(50)
+turtle.left(90)
+turtle.fd(50)
+turtle.left(90)
+turtle.fd(50)
+turtle.right(90)
+turtle.fd(50)
+turtle.left(90)
+turtle.fd(50)
+turtle.left(90)
+turtle.fd(50)
+turtle.right(90)
+turtle.fd(50)
+turtle.left(90)
+turtle.fd(50)
+turtle.left(90)
+turtle.fd(50)
+turtle.right(90)
+turtle.fd(50)
+turtle.left(90)
+
+turtle.done()
+
+
