6dd6585123dbd9cc4af325011f17c5e2aba9ee16
andremeronen1 <andre@hkhk.edu.ee>
2024-10-16 11:27:39 +0300

    Create harjutus3.py

---
+++ harjutus3.py
@@ -0,0 +1,67 @@
+#16.10.24 Meronen
+#Ülesanne 3
+import turtle
+
+#Ülesanne 3.2 Ostunimekiri
+toode = "apelsine"
+kogus = 23
+hind = 0.78
+print("Soovin",toode,"mille tüki hind on",hind, "eurot")
+
+
+
+
+
+"""
+#Ülesanne 3.4 Lemmikraamat
+raamatu_pealkiri = "lord of the rings"
+raamatu_autor = "J.R.R Tolkien"
+raamat = raamatu_pealkiri+" "+raamatu_autor
+lehekylgede_arv = 1137
+hindamisskoor = 8.9
+print("Minu lemmik raamat on",raamatu_pealkiri,raamatu_autor,"mis on", lehekylgede_arv, "pikk ja mille ma hindan",hindamisskoor,"punktiga")
+"""
+
+
+
+
+
+"""
+#Ülesanne 3.5 Unistuste auto
+mark, mudel = "datsun", "100a"
+auto = mark+" "+mudel
+aasta = 1974
+hind = 350
+
+print("Minu unistuste auto on",auto, "aastast",aasta,", mille hind on",hind,"eurot")
+"""
+
+
+
+
+
+"""
+#Ülesanne 3.7 python turtle kolmnurk
+kylje_pikkus = 100
+nurk = 120
+kujundi_varv = "green"
+
+turtle.pencolor(kujundi_varv)
+
+for i in range (3):
+    for i in range(3):
+        turtle.fd(kylje_pikkus)
+        turtle.left(nurk)
+
+    turtle.penup()
+    turtle.fd(kylje_pikkus*1.5)
+    turtle.pendown()
+"""
+
+
+
+
+
+
+
+turtle.done
