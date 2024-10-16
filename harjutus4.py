cdd15893cbf9d27ab5e2b61c75ec8e3a5df682a8
andremeronen1 <andre@hkhk.edu.ee>
2024-10-16 11:46:01 +0300

    Create harjutus4.py

---
+++ harjutus4.py
@@ -0,0 +1,19 @@
+#16.10.24 Andre Meronen
+#Ülesanne 4
+
+
+# 2. Raamatute allahindlus
+ale = 0.3
+kogus = 3
+hind = 12.53
+summa = hind - (hind * ale) * kogus
+print(f"3 raamatu hind soodusega on {summa:0.2f}€")
+
+
+"""
+# 1. Aia pikkus
+a = int(input( "Sisesta külg 1: "))
+b = int(input( "Sisesta külg 2: "))
+p = 2 * (a + b)
+print(f"Aia kogupikkus on {p} meetrit.")
+"""
